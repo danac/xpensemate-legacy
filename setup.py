abe64be5eb594c780658733ddce5f174005112a7
Dana Christen <dana.christen@epfl.ch>
2013-04-23 22:30:31 +0200

    Added setup.py + removed __init__ in tests

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='expense_manager',
+    version='0.1',
+    packages=['expense_manager.core', 'expense_manager.db'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+)
