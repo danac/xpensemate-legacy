7c0193b1e1daf8441c35c5068810471e38a1279b
Dana Christen <dana.christen@epfl.ch>
2013-04-24 19:34:03 +0200

    Brought back init in tests and refined TestDB

--- tests/TestDB.py
+++ tests/TestDB.py
@@ -3,23 +3,27 @@
 
 from nose.tools import assert_equals, assert_raises
 from expense_manager.db import DBInterface
+import shutil
 
-class TestDB:
+class TestDBSQLite:
 
     @classmethod
     def setupClass(cls):
         pass
 
     def setUp(self):
+        self.interface_memory = DBInterface("")
         pass
 
     def tearDown(self):
         pass
 
-    def testFirstTry(self):
-        interface = DBInterface("expense_test.db")
-        interface.createStructure()
-        balances = interface.getOpenBalances()
-        for balance in balances:
-            print(balance)
+    def test_structure(self):
+        self.interface_memory.createStructure()
+
+    def test_add_query_expense(self):
+        pass
+
+    def test_query_open_balance(self):
+        pass
 
